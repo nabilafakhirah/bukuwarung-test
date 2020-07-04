0bc1e28690ff125c32ac3051fa6cb4724b8b8cc4
Nabila Fakhirah <nabilafakhirah@gmail.com>
2020-07-05 00:15:50 +0700

    made bottom navigation menu

--- app/src/main/java/com/example/bukuwarung/MainActivity.java
+++ app/src/main/java/com/example/bukuwarung/MainActivity.java
@@ -1,8 +1,9 @@
 package com.example.bukuwarung;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +12,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.navigation,menu);
+        return true;
+    }
+
 }
