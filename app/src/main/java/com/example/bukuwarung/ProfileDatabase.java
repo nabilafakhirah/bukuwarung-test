6c218625d51f0ca29cc447bfa30cce5672c025c1
Nabila Fakhirah <nabilafakhirah@gmail.com>
2020-07-06 07:39:25 +0700

    populate initial database

--- app/src/main/java/com/example/bukuwarung/ProfileDatabase.java
+++ app/src/main/java/com/example/bukuwarung/ProfileDatabase.java
@@ -1,9 +1,12 @@
 package com.example.bukuwarung;
 
+import android.arch.persistence.db.SupportSQLiteDatabase;
 import android.arch.persistence.room.Database;
 import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
 import android.content.Context;
+import android.os.AsyncTask;
+import android.support.annotation.NonNull;
 
 @Database(entities = {Profile.class}, version = 1)
 public abstract class ProfileDatabase extends RoomDatabase {
@@ -16,8 +19,31 @@ public abstract class ProfileDatabase extends RoomDatabase {
         if(instance==null) {
             instance = Room.databaseBuilder(context.getApplicationContext(),
                     ProfileDatabase.class, "profile_database")
-                    .fallbackToDestructiveMigration().build();
+                    .fallbackToDestructiveMigration()
+                    .addCallback(roomCallback).build();
         }
         return instance;
+    }
+
+    private static RoomDatabase.Callback roomCallback = new RoomDatabase.Callback() {
+        @Override
+        public void onCreate(@NonNull SupportSQLiteDatabase db) {
+            super.onCreate(db);
+            new PopulateDbAsyncTask(instance).execute();
+        }
+    };
+
+    private static class PopulateDbAsyncTask extends AsyncTask<Void, Void, Void> {
+        private ProfileDao profileDao;
+
+        public PopulateDbAsyncTask(ProfileDatabase db) {
+            this.profileDao = db.profileDao();
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            profileDao.insert(new Profile("Name", "email@mail.com", "Undefined"));
+            return null;
+        }
     }
 }
